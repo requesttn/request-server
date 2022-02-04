a1e97534510c7ccd23b2831f0587d66f84443b24
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-05 00:04:16 +0100

    Ignore .idea

---
+++ ri-data/src/test/java/tn/request/data/RiDataApplicationTests.java
@@ -0,0 +1,12 @@
+package tn.request.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
