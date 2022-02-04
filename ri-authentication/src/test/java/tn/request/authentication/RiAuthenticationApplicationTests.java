540e906e21c8c7f8bddc00c67528ab5e8825bda3
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-04 23:47:41 +0100

    Refactor project to multi modular

---
+++ ri-authentication/src/test/java/tn/request/authentication/RiAuthenticationApplicationTests.java
@@ -0,0 +1,12 @@
+package tn.request.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ ri-data/src/main/java/tn/request/data/RiDataApplication.java
@@ -0,0 +1,12 @@
+package tn.request.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiDataApplication.class, args);
+    }
+}
