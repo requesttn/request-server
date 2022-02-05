ce999e27181b0c9127729b3495665c2dcbdb06ad
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-05 18:18:58 +0100

    Call registration from the application module

--- ri-app/src/main/java/tn/request/app/MainApp.java
+++ ri-app/src/main/java/tn/request/app/MainApp.java
@@ -1,15 +1,28 @@
 package tn.request.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tn.request.authentication.user.UserRegistrationData;
+import tn.request.authentication.user.UserRegistrationService;
 
 @SpringBootApplication
+@EnableJpaRepositories("tn.request.*")
+@EntityScan("tn.request.*")
 @RestController
 @RequestMapping("/")
-public class MainApp {
+@ComponentScan(basePackages = "tn.request.*")
+public class MainApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRegistrationService userRegistrationService;
 
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
@@ -18,5 +31,12 @@ public class MainApp {
     @GetMapping
     public String getMessage() {
         return "<h1> Hello World </h1>";
+    }
+
+    @Override
+    public void run(String... args) {
+        userRegistrationService.registerUser(new UserRegistrationData(
+                "Houssem", "Nasri", "houssemnasri2001@gmail.com"
+        ));
     }
 }
