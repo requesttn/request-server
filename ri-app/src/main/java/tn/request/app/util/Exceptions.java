f60417426c6214d7be17e0454017fef70db98d24
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-05 23:16:20 +0100

    Add a utility class for throwing exceptions

---
+++ ri-app/src/main/java/tn/request/app/util/Exceptions.java
@@ -0,0 +1,23 @@
+package tn.request.app.util;
+
+public class Exceptions {
+    public static <E extends Exception> void throwIf(boolean condition, E exception) throws E {
+        if (condition) {
+            throw exception;
+        }
+    }
+
+    public static void throwIllegalArgumentIf(boolean condition, String message)
+            throws IllegalArgumentException {
+        if (condition) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void throwIllegalArgumentIf(
+            boolean condition, String format, Object... args) throws IllegalArgumentException {
+        if (condition) {
+            throw new IllegalArgumentException(String.format(format, args));
+        }
+    }
+}
