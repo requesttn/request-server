d16f201288750b93ed6debae68c356da91628691
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-06 01:17:17 +0100

    Disable csrf protection

--- ri-app/src/main/java/tn/request/app/InvalidEmailFormatException.java
+++ ri-app/src/main/java/tn/request/app/InvalidEmailFormatException.java
@@ -1,7 +1,5 @@
 package tn.request.app;
 
-import lombok.experimental.StandardException;
-
 public class InvalidEmailFormatException extends Exception {
     public InvalidEmailFormatException(String message) {
         super(message);
