2bc599acce62ffcc68624ad53b05d114926e867c
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-06 02:00:38 +0100

    Add ConfirmationTokenEntity

---
+++ ri-data/src/main/java/tn/request/data/ConfirmationTokenEntity.java
@@ -0,0 +1,59 @@
+package tn.request.data;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import tn.request.data.user.UserEntity;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class ConfirmationTokenEntity {
+    private static final long EXPIRES_AFTER_N_MINUTES = 24 * 60;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private UserEntity user;
+
+    private LocalDateTime expiryDate = computeExpiryDate();
+
+    private static LocalDateTime computeExpiryDate() {
+        return LocalDateTime.now().plusMinutes(EXPIRES_AFTER_N_MINUTES);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+            return false;
+        ConfirmationTokenEntity that = (ConfirmationTokenEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
