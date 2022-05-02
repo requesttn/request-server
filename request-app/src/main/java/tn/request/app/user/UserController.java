2019a1f2f47e3456e28cbab9f3e501c3ae4201a8
Houssem Nasri <housi.housi2015@gmail.com>
2022-05-02 01:06:48 +0100

    Return 'User' instead of 'UserDto' from UserService

--- request-app/src/main/java/tn/request/app/user/UserController.java
+++ request-app/src/main/java/tn/request/app/user/UserController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tn.request.service.auth.model.User;
 import tn.request.service.user.UserService;
-import tn.request.service.user.dto.UserDto;
 
 @AllArgsConstructor
 @Slf4j
@@ -20,7 +20,7 @@ public class UserController implements IUserController {
 
     @Override
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 }
--- request-service/src/main/java/tn/request/service/user/UserService.java
+++ request-service/src/main/java/tn/request/service/user/UserService.java
@@ -6,16 +6,19 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import tn.request.bazooka.opt.BazookaOpt;
 import tn.request.data.user.UserRepository;
+import tn.request.service.auth.mapper.UserMapper;
+import tn.request.service.auth.model.User;
 import tn.request.service.question.exception.RequestException;
-import tn.request.service.user.dto.UserDto;
 
 @AllArgsConstructor
 @Service
 public class UserService {
+
+    private UserMapper userMapper;
     private UserRepository userRepository;
 
-    public UserDto getUserById(@NonNull Long id) {
-        return UserDto.fromEntity(BazookaOpt.checkIfEmpty(userRepository.findById(id))
+    public User getUserById(@NonNull Long id) {
+        return userMapper.fromUserEntity(BazookaOpt.checkIfEmpty(userRepository.findById(id))
                 .thenThrow(new RequestException(HttpStatus.NOT_FOUND, "The provided id doesn't correspond to any user"))
                 .orElseGet());
     }
